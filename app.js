36235b7dce38156404600ae3c2271c5981fb0b33
Harsh pal <113094328+Harsh17022004@users.noreply.github.com>
2023-12-22 18:09:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -107,24 +107,3 @@ gsap.from(".footer .footer_inn .box", {
     stagger: 0.3
 })
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
